e9990d700765b0e1989edaa8871d807c589cf5c9
Dyvap <dyvap@hotmail.com>
2024-11-27 07:10:47 +0100

    Updates for pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Registro del Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    try {
+      navigator.serviceWorker.register('/fachometro/sw.js').then(registration => {
+        console.log('ServiceWorker registration successful:', registration.scope);
+      }).catch(error => {
+        console.log('ServiceWorker registration failed:', error);
+      });
+    } catch (error) {
+      console.log('ServiceWorker registration failed:', error);
+    }
+  });
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
