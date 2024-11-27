1d66270a482dddb9c61b73f80dee561ff1fda610
Dyvap <dyvap@hotmail.com>
2024-11-27 08:41:15 +0100

    New SW test

--- public/sw.js
+++ public/sw.js
@@ -2,19 +2,60 @@ const CACHE_NAME = 'fachometro-v1';
 const urlsToCache = [
   '/fachometro/',
   '/fachometro/index.html',
-  '/fachometro/manifest.json'
+  '/fachometro/manifest.json',
+  '/fachometro/assets/index-BqC7zz0i.js',   // Asegúrate de actualizar estos nombres con cada nueva versión
+  '/fachometro/assets/index-B6vGBxOe.css'   // Asegúrate de actualizar estos nombres con cada nueva versión
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).then(response => {
+      // Asegúrate de que obtienes una respuesta válida.
+      if (!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+
+      const responseToCache = response.clone();
+
+      caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, responseToCache);
+      });
+
+      return response;
+    }).catch(error => {
+      // Intenta recuperar el archivo desde la caché si la red falla.
+      return caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        throw error;  // Lanza el error si no hay nada en caché y la red falla.
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
